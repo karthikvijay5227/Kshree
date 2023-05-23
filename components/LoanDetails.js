73546183d1872471f9260effa491df77ea2c2bf2
M3BIONIX <sanjay.mathewofficial2020@gmail.com>
2023-05-23 23:27:32 +0530

    Admin Screen completed woohoo

--- components/LoanDetails.js
+++ components/LoanDetails.js
@@ -1,22 +1,129 @@
 import * as React from 'react';
-import { View, StyleSheet,Text } from 'react-native';
+import { View, Text, TouchableOpacity, Dimensions, ScrollView,StyleSheet } from 'react-native';
+import { createClient } from '@supabase/supabase-js';
+import { CountUp } from 'use-count-up';
+import { createStackNavigator } from '@react-navigation/stack';
+import LoanUser from '../components/LoanUser';
+
+const height = Dimensions.get('window').height;
+const width = Dimensions.get('window').width;
+
+const Stack = createStackNavigator();
 
 export default class LoanDetails extends React.Component {
     render(){
         return(
-            <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-                <Text>LoanDetails</Text>
-            </View>
+            <Stack.Navigator initialRouteName="LoanInfo" screenOptions={{headerShown : false}}>
+                <Stack.Screen name='LoanInfo' component={LoanInfo} />
+                <Stack.Screen name='LoanUser' component={LoanUser} />
+            </Stack.Navigator>
         )
     }
 }
 
 
+class LoanInfo extends React.Component {
+    
+    constructor(props){
+        super(props);
+        this.state = {
+            totalAmount : '',
+            loanMembers : [],
+            color : [].fill('#38E038')
+        }
+    }
+
+    componentDidMount(){
+
+       const { navigation } = this.props;
+       this.onNavigate = navigation.addListener('focus', async() => {
+        const supabaseUrl = 'https://axubxqxfoptpjrsfuzxy.supabase.co'
+        const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImF4dWJ4cXhmb3B0cGpyc2Z1enh5Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY4MTc1NTM4NSwiZXhwIjoxOTk3MzMxMzg1fQ.SWDMCer4tBPEVNfrHl1H0iJ2YiWJmitGtJTT3B6eTuA'
+        const supabase = createClient(supabaseUrl, supabaseKey)
+        this.setState({totalAmount : await supabase.rpc('calculate_total_amount')})
+        let { data: obj1} = await supabase.rpc('calculate_monthly_payment')
+        this.setState({loanMembers : obj1})
+        
+
+       }) 
+        
+    }
+    
+    
+    render(){
+
+        const amount = parseInt(this.state.totalAmount.data)
+        
+        const handleLoanDetails = () => {
+
+            const { navigation } = this.props;
+
+            return (
+                this.state.loanMembers.map((item,index) => {
+
+                    return(
+                        <TouchableOpacity key={index} activeOpacity={0.8} style={styles.names} onPress={()=> navigation.navigate('LoanUser', {name : item.username})}>
+                        <View style={{ flexDirection: 'row' }}>
+                          <Text style={{ fontFamily: 'Outfit-SemiBold', fontSize: 15, marginLeft: 10 }}>{index + 1}.</Text>
+                          <Text style={{ fontFamily: 'Outfit-SemiBold', fontSize: 15, marginLeft: 10 }}>{item.username}</Text>
+                        </View>
+                        <View style={{ flexDirection: 'column', marginRight: 20 }}>
+                          <Text style={{ fontFamily: 'Outfit-Bold', fontSize: 15, color: '#38E038' }}>₹ {Math.round(item.monthly_payment).toFixed(2)}</Text>
+                          <Text style={{ fontFamily: 'Outfit-Bold', fontSize: 15, color: new Date(item.date).toISOString().slice(0,10) < new Date().toISOString().slice(0,10) ? 'red' : '#38E038' }}>{item.date}</Text>
+                        </View>
+                      </TouchableOpacity>
+                      
+
+                    )
+                })
+            )
+        }
+
+        return(
+            <View style={{ flex: 1, justifyContent: 'flex-start', alignItems: 'flex-start', backgroundColor : 'white'}}>
+                    <View style={{flexDirection : 'row'}}>
+                        <TouchableOpacity disabled style={{elevation : 10,shadowOffset : 3,backgroundColor : 'white', borderRadius : 20,marginLeft : 20, marginTop : 30 ,width : width - 180, height : 100, justifyContent : 'flex-start', alignItems : 'flex-start'}}>
+                            <Text style={{fontSize : 15, fontFamily : 'Outfit-SemiBold', marginTop : 20, marginLeft : 20 }}>Total Amount</Text>
+                            <Text style={{fontFamily : 'Outfit-Bold', fontSize : 30, color : '#38E038', alignSelf : 'flex-end', marginRight : 30}}>                 
+                                ₹ <CountUp isCounting end={amount} duration={3} />
+                            </Text>
+                        </TouchableOpacity>
+
+                        <TouchableOpacity disabled style={{elevation : 10,shadowOffset : 3,backgroundColor : 'white', borderRadius : 20,marginLeft : 10, marginTop : 30 ,width : width - 260, height : 100, justifyContent : 'flex-start', alignItems : 'flex-start'}}>
+                            <Text style={{fontSize : 15, fontFamily : 'Outfit-SemiBold', marginTop : 20, marginLeft : 20 }}>Borrowers</Text>
+                            <Text style={{fontFamily : 'Outfit-Bold', fontSize : 30, color : '#38E038', alignSelf : 'flex-end', marginRight : 30}}>                 
+                                 <CountUp isCounting end={this.state.loanMembers.length} duration={3} />
+                            </Text>
+                        </TouchableOpacity>
+
+                     </View>
+
+                <ScrollView style={{marginTop : 30}} contentContainerStyle={{paddingBottom : 20,marginLeft : 30, width : width - 50, alignItems : 'center'}}>
+                
+                 {
+                    handleLoanDetails()
+                 }
+                 </ScrollView> 
+                
+            </View>
+        )
+    }
+}
+
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        justifyContent : 'center',
-        alignItems : 'center'
+    names : { 
+        flexDirection: 'row', 
+        height: 60, 
+        justifyContent: 'space-between', 
+        alignItems: 'center', 
+        marginTop: 20, 
+        elevation: 5, 
+        shadowOffset: { width: 5, height: 2 }, 
+        width: width - 70, 
+        shadowOpacity: 0.5, 
+        shadowRadius: 3, 
+        backgroundColor: 'white', 
+        padding: 10, 
+        borderRadius: 10 
     }
 })
---
+++ components/LoanUser.js
@@ -0,0 +1,90 @@
+import React from "react";
+import { View, Text, TouchableOpacity, Image, Dimensions } from "react-native";
+import { createClient } from '@supabase/supabase-js';
+
+
+const height = Dimensions.get('window').height;
+const width = Dimensions.get('window').width;
+
+export default class LoanUser extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      loanDetails: [],
+      monthlyPayment: ''
+    }
+  }
+
+  async componentDidMount() {
+    const supabaseUrl = 'https://axubxqxfoptpjrsfuzxy.supabase.co'
+    const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImF4dWJ4cXhmb3B0cGpyc2Z1enh5Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY4MTc1NTM4NSwiZXhwIjoxOTk3MzMxMzg1fQ.SWDMCer4tBPEVNfrHl1H0iJ2YiWJmitGtJTT3B6eTuA'
+    const supabase = createClient(supabaseUrl, supabaseKey)
+    let { data: obj1 } = await supabase.from('loan').select('*').eq('username', this.props.route.params.name)
+    this.setState({ loanDetails: obj1 })
+    let name = this.props.route.params.name
+    let { data, error } = await supabase.rpc('monthly_payment', {p_username : name})
+    this.setState({monthlyPayment : data}) 
+    console.log(this.state.monthlyPayment)
+  }
+
+  render() {
+    return (
+      <View style={{ flex: 1, justifyContent: 'flex-start', alignContent: 'flex-start', backgroundColor: 'white' }}>
+        <View style={{ flexDirection: 'row', justifyContent: 'flex-start', alignItems: 'center', padding: 10 }}>
+          <TouchableOpacity style={{ marginLeft: 20, marginTop: 20, justifyContent: 'center', alignContent: 'center', elevation: 8, width: 45, height: 45, borderRadius: 50, backgroundColor: 'white', shadowOffset: { width: 2, height: 2 }, shadowOpacity: 0.5, shadowRadius: 5 }} onPress={() => this.props.navigation.goBack()}>
+            <Image source={require('../assets/arrow-left.png')} style={{ height: 20, width: 20, alignSelf: 'center' }} />
+          </TouchableOpacity>
+          <Text style={{ fontFamily: 'InterTight-Bold', fontSize: 30, color: 'black', marginLeft: 20, marginTop: 15 }}>Loan Users</Text>
+        </View>
+        <Text style={{ fontFamily: 'Outfit-SemiBold', fontSize: 20, color: '#1A1110', marginTop: 30, marginLeft: 20 }}>Details</Text>
+        {this.state.loanDetails.length > 0 && (
+          <TouchableOpacity style={{ elevation: 10, shadowOffset: 3, backgroundColor: 'white', borderRadius: 20, marginLeft: 20, marginTop: 30, width: width - 40, height: height - 280 }}>
+           <View style={{ flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center' }}>
+                <View style={{ flexDirection: 'column' }}>
+                    <Text style={{ fontSize: 18, fontFamily: 'Outfit-SemiBold', marginTop: 20, marginLeft: 20, color: '#1A1110' }}>Simple Credit</Text>
+                    <Text style={{ fontSize: 13, fontFamily: 'Outfit-SemiBold', marginTop: 5, marginLeft: 20, color: '#808080' }}>Cash Loan</Text>
+                    <Text style={{ fontSize: 15, fontFamily: 'Outfit-SemiBold', marginTop: 5, marginLeft: 20, color: '#1A1110' }}>{this.state.loanDetails[0]?.bank}</Text>
+                </View>
+
+                <View style={{ justifyContent : 'center', alignContent : 'center', borderRadius : 50, borderWidth : 2, height : 60, width : 60, marginRight : 30 }}>
+                    <Image source={require('../assets/money.png')} style={{ height: 30, width: 30, alignSelf : 'center' }} />
+                </View>
+            </View>
+            <View style={{height : 1.2, width : width - 40, backgroundColor : '#d3d3d3', marginTop : 20, borderRadius : 20}}/>
+            <View style={{ flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center' }}>
+                <View style={{ flexDirection: 'column' }}>
+                    <Text style={{ fontSize: 15, fontFamily: 'Outfit-SemiBold', marginTop: 20, marginLeft: 20, color: 'grey' }}>Loan Amount</Text>
+                    <Text style={{ fontSize: 25, fontFamily: 'Outfit-SemiBold', marginTop: 2, marginLeft: 20, color: 'black' }}>₹ {this.state.loanDetails[0]?.amount}</Text>
+                    <Text style={{ fontSize: 15, fontFamily: 'Outfit-SemiBold', marginTop: 20, marginLeft: 20, color: 'grey' }}>Intrest Rate</Text>
+                    <Text style={{ fontSize: 20, fontFamily: 'Outfit-SemiBold', marginTop: 2, marginLeft: 20, color: 'black' }}>{this.state.loanDetails[0]?.rate}%</Text>
+                </View>
+                <View style={{ flexDirection: 'column', marginRight : 30 }}>
+                    <Text style={{ fontSize: 15, fontFamily: 'Outfit-SemiBold', marginTop: 20, marginLeft: 20, color: 'grey' }}>Monthly Payment</Text>
+                    <Text style={{ fontSize: 25, fontFamily: 'Outfit-SemiBold', marginTop: 2, marginLeft: 20, color: 'black' }}>₹ {Math.round(this.state.monthlyPayment).toFixed(2)} </Text>
+                    <Text style={{ fontSize: 15, fontFamily: 'Outfit-SemiBold', marginTop: 20, marginLeft: 20, color: 'grey' }}>Duration</Text>
+                    <Text style={{ fontSize: 20, fontFamily: 'Outfit-SemiBold', marginTop: 2, marginLeft: 20, color: 'black' }}>{this.state.loanDetails[0]?.duration} months</Text>
+                </View>
+            </View>
+                
+                <View style={{height : 1.2, width : width - 40, backgroundColor : '#d3d3d3', marginTop : 20, borderRadius : 20}}/>
+                
+            <View style={{ flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center' }}>
+                <View style={{ flexDirection: 'column', marginRight : 30 }}>
+                    <Text style={{ fontSize: 15, fontFamily: 'Outfit-SemiBold', marginTop: 20, marginLeft: 20, color: 'grey' }}>Account Number</Text>
+                    <Text style={{ fontSize: 16, fontFamily: 'Outfit-SemiBold', marginTop: 2, marginLeft: 20, color: 'black' }}>{this.state.loanDetails[0]?.account} </Text>
+                    <Text style={{ fontSize: 15, fontFamily: 'Outfit-SemiBold', marginTop: 20, marginLeft: 20, color: 'grey' }}>IFSC code</Text>
+                    <Text style={{ fontSize: 18, fontFamily: 'Outfit-SemiBold', marginTop: 2, marginLeft: 20, color: 'black' }}>{this.state.loanDetails[0]?.ifsc}</Text>
+                </View>
+                <View style={{ flexDirection: 'column', marginRight : 50 }}>
+                    <Text style={{ fontSize: 15, fontFamily: 'Outfit-SemiBold', marginTop: 20, marginLeft: 20, color: 'grey' }}>Branch</Text>
+                    <Text style={{ fontSize: 16, fontFamily: 'Outfit-SemiBold', marginTop: 2, marginLeft: 20, color: 'black' }}>{this.state.loanDetails[0]?.branch} </Text>
+                </View>
+            </View>
+            
+           </TouchableOpacity>
+        )}
+      </View>
+    )
+  }
+}
