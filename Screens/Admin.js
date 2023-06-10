bed6128be9912891516811a128863a076138e8b9
Karthik Vijay <kv52274@gmail.com>
2023-06-10 20:23:56 +0530

    Minor bug fixes

--- Screens/Admin.js
+++ Screens/Admin.js
@@ -121,7 +121,7 @@ function AdminHome() {
             } else if (day === 2 || day === 22) {
                 return 'nd';
             } else if (day === 3 || day === 23) {
-                return day + 'rd';
+                return 'rd';
             } else {
                 return 'th';
             }
--- Screens/Members.js
+++ Screens/Members.js
@@ -151,7 +151,7 @@ function MemberHome({ navigation, username }) {
         } else if (day === 2 || day === 22) {
             return 'nd';
         } else if (day === 3 || day === 23) {
-            return day + 'rd';
+            return 'rd';
         } else {
             return 'th';
         }
