b736430d73f758be0b0b1243999e31693c0fe89c
M3BIONIX <you@example.com>
2023-07-29 00:33:07 +0530

    Dynamic User Deletion

--- components/UserDetails.js
+++ components/UserDetails.js
@@ -1,12 +1,13 @@
 import * as React from 'react';
-import { View, Text, TouchableOpacity, Dimensions, ScrollView, StyleSheet, BackHandler,Image } from 'react-native';
+import { View, Text, TouchableOpacity, Dimensions, ScrollView,Image, Alert } from 'react-native';
 import { createClient } from '@supabase/supabase-js';
-import { Switch, TextInput } from 'react-native-paper';
+import { Paragraph, Switch, TextInput } from 'react-native-paper';
 import { createStackNavigator } from '@react-navigation/stack';
 import { IconButton } from 'react-native-paper';
 import Lottie from 'lottie-react-native';
 import { SelectList } from 'react-native-dropdown-select-list';
-import ToggleButton from 'react-native-paper';
+import { Modal } from 'react-native';
+import { RefreshControl } from 'react-native-gesture-handler';
 
 
 const { width, height } = Dimensions.get('window');
@@ -53,7 +54,13 @@ class Users extends React.Component{
         super(props);
         this.state = {
             users : [],
-            refresh : false
+            refresh : false,
+            deleteUser : false,
+            error : false,
+            deleteName : '',
+            viewWidth : 40,
+            delText : '',
+            deleteUserName : ''
         }
     }
     
@@ -61,8 +68,8 @@ class Users extends React.Component{
                 const supabaseUrl = 'https://axubxqxfoptpjrsfuzxy.supabase.co';
                 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImF4dWJ4cXhmb3B0cGpyc2Z1enh5Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY4MTc1NTM4NSwiZXhwIjoxOTk3MzMxMzg1fQ.SWDMCer4tBPEVNfrHl1H0iJ2YiWJmitGtJTT3B6eTuA';
                 const supabase = createClient(supabaseUrl, supabaseKey);
-                    let {data : data} = await supabase.from('users').select('*');
-                    this.setState({users : data});
+                let {data : data} = await supabase.from('users').select('*');
+                this.setState({users : data});
     }
     
     render(){
@@ -70,6 +77,7 @@ class Users extends React.Component{
         const displayUsers = () => {
            
             const {navigation} = this.props;
+          
 
             return(
             this.state.users.map((item, index) => {
@@ -85,7 +93,10 @@ class Users extends React.Component{
                                 </View>
                                 <View style={{justifyContent : 'flex-start',alignItems : 'center', }}>
                                   <View style={{flexDirection : 'row', marginRight : 10}}>
-                                  <IconButton icon={require('../assets/delete.png')} size={20} onPress={()=>{}} style={{ elevation : 10}}/>
+                                  <IconButton icon={require('../assets/delete.png')} size={20} onPress={()=>{
+                                    this.setState({deleteUser : true})
+                                    this.setState({deleteName : item.name,deleteUserName : item.username})
+                                    }} style={{ elevation : 10}}/>
                                   <IconButton icon={require('../assets/edit.png')} size={20} onPress={()=>{navigation.navigate("Edit User",{
                                         name : item.name,
                                         username : item.username,
@@ -110,23 +121,114 @@ class Users extends React.Component{
                 const supabaseUrl = 'https://axubxqxfoptpjrsfuzxy.supabase.co';
                 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImF4dWJ4cXhmb3B0cGpyc2Z1enh5Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY4MTc1NTM4NSwiZXhwIjoxOTk3MzMxMzg1fQ.SWDMCer4tBPEVNfrHl1H0iJ2YiWJmitGtJTT3B6eTuA';
                 const supabase = createClient(supabaseUrl, supabaseKey);
-                let {data : data} = await supabase.from('users').select('name');
+                let {data : data} = await supabase.from('users').select('*');
                 this.setState({users : data});
-                
+                this.setState({refresh : false})
         }
         
         const {navigation} = this.props;
         
+
+        const deleteUser = async() => {
+          
+                const supabaseUrl = 'https://axubxqxfoptpjrsfuzxy.supabase.co';
+                const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImF4dWJ4cXhmb3B0cGpyc2Z1enh5Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY4MTc1NTM4NSwiZXhwIjoxOTk3MzMxMzg1fQ.SWDMCer4tBPEVNfrHl1H0iJ2YiWJmitGtJTT3B6eTuA';
+                const supabase = createClient(supabaseUrl, supabaseKey);
+                if(this.state.delText === 'Delete User')
+                {
+                    console.log('User is Being Deleted')
+                    
+                    try{
+                    await supabase.from('users').delete().eq('username',this.state.deleteUserName).then(
+                        ()=>{
+                        this.setState({deleteUser : false})
+                        Alert.alert('Deletion Successful',`Deletion of username : ${this.state.deleteName} was Successful`)
+                        onRefresh()
+
+                        const channel = supabase.channel('notif');
+                        const task = {username : this.state.deleteUserName}
+                        channel.subscribe((status) => {
+                            if (status === 'SUBSCRIBED') {
+                                channel.send({
+                                    type: 'broadcast',
+                                    event: 'delete',
+                                    payload: {task}
+                                })
+                            }
+                        })
+
+                        }
+                    )}
+                    catch{
+                        this.setState({error : true})
+                    }
+                }
+                else {
+                    this.setState({error : true})
+                }
+
+                
+                
+        }
+        
         return(
             
             <View style={{backgroundColor : 'white', flex : 1}}>
+               
+               <View>
+                    <Modal
+                        animationType="fade"
+                        transparent={true}
+                        visible={this.state.deleteUser}
+                        onRequestClose={() => {
+                            this.setState({deleteUser : !this.state.deleteUser})
+                            }}
+                        >
+                        <View style={{flex :1,justifyContent : 'center', alignItems :'center',backgroundColor: 'rgba(45, 45, 45, 0.5)'}}>
+                            <TouchableOpacity disabled style={{backgroundColor : 'white',height : height/2 - 10, width : width - 100, borderRadius : 20, elevation : 6,justifyContent : 'flex-start',alignItems : 'center'}}>
+                                <View style={{flexDirection : 'row', justifyContent : 'center',marginTop : 30}} >
+                                    <Image source={require('../assets/warning.png')} style={{height : 40, width : 40}}/>
+                                    <Text style={{fontFamily : 'InterTight-Bold', color : '#454545',fontSize : 20, marginLeft : 10, marginTop : 5}}>Warning!</Text>
+                                </View>
+                            
+                                <Text style={{fontFamily : 'Outfit-Medium', fontSize : 15, marginTop : 30, width : 250}}>
+                                    You are about to delete the user : <Text style={{fontFamily : 'Outfit-Bold', fontSize : 15, marginTop : 5, width : 'auto'}}>{this.state.deleteName}</Text>
+                                </Text>
+                                
+                                <Text style={{fontFamily : 'Outfit-Medium', fontSize : 15, marginTop : 5, width : 250}}>
+                                    To make sure that you are deleting the user type <Text style={{fontFamily : 'Outfit-Bold', fontSize : 15, marginTop : 5, width : 'auto', color : 'red'}}> Delete User</Text>
+                                    <Text style={{fontFamily : 'Outfit-Medium', fontSize : 15}}> in the below text feild</Text>
+                                </Text>
+                                
+                                <TextInput 
+                                  label={'Delete User'}
+                                  placeholder='Type in Delete User'
+                                  mode='outlined'
+                                  error={this.state.error}
+                                  value={this.state.delText}
+                                  onChangeText={text => this.setState({ delText: text })}
+                                  style={{ width: 250, marginTop: 20, alignSelf: 'center', backgroundColor : 'white' }}
+                                />                              
+                                
+                                <TouchableOpacity style={{width : 250, height : 40,marginTop : 20, backgroundColor : '#D0312D', justifyContent : 'center', alignItems : 'center', borderRadius : 5}} onPress={()=>{deleteUser()}}>
+                                    <Text style={{fontFamily : 'Outfit-Bold', fontSize : 15, color : 'white'}}>Delete User</Text>
+                                </TouchableOpacity>
+                               
+                                    
+                            </TouchableOpacity>
+                        </View>
+                    </Modal>
+                </View>
+                
+                
+                
                 <View style={{ flexDirection: 'row', justifyContent: 'flex-start', alignItems: 'center', padding: 10, marginTop: 20, width: width }}>
                     <TouchableOpacity style={{ marginLeft: 20, marginTop: 15, justifyContent: 'center', alignContent: 'center', elevation: 8, width: 45, height: 45, borderRadius: 50, backgroundColor: 'white', shadowOffset: { width: 2, height: 2 }, shadowOpacity: 0.5, shadowRadius: 5 }} onPress={() => this.props.navigation.goBack()}>
                         <Image source={require('../assets/arrow-left.png')} style={{ height: 20, width: 20, alignSelf: 'center' }} />
                     </TouchableOpacity>
                     <Text style={{ fontFamily: 'InterTight-Bold', fontSize: 30, color: 'black', marginTop: 15, marginLeft: 25 }}>User Details</Text>                
                 </View>
-                <ScrollView>
+                <ScrollView refreshControl={<RefreshControl refreshing={this.state.refresh} onRefresh={()=>{onRefresh()}} />}>
                         {displayUsers()}
                 </ScrollView>
             </View>
@@ -142,7 +244,6 @@ class EditUser extends React.Component {
         this.state = {
             name : this.props.route.params.name,
             username : this.props.route.params.username,
-            password : this.props.route.params.password,
             admin : this.props.route.params.admin,
             address : this.props.route.params.address,
             phone : this.props.route.params.phone,
@@ -156,6 +257,44 @@ class EditUser extends React.Component {
                
         const width = Dimensions.get('window').width;
 
+        const {navigation} = this.props;
+
+        const updateData = async(uname, uusername, uaddress, uadmin, uphone ) => {
+                const supabaseUrl = 'https://axubxqxfoptpjrsfuzxy.supabase.co';
+                const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImF4dWJ4cXhmb3B0cGpyc2Z1enh5Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY4MTc1NTM4NSwiZXhwIjoxOTk3MzMxMzg1fQ.SWDMCer4tBPEVNfrHl1H0iJ2YiWJmitGtJTT3B6eTuA';
+                const supabase = createClient(supabaseUrl, supabaseKey);
+                await supabase.from('users').update([
+                    {
+                        name : this.state.name,
+                        username : this.state.username,
+                        password : this.state.password,
+                        admin : this.state.admin,
+                        address : this.state.address,
+                        phone_number : this.state.phone
+
+                    }
+                ]).eq('username', this.state.username);
+
+                
+                const channel = supabase.channel('KshreeDB').on('postgres_changes',
+                    {event: 'UPDATE', schema: 'public', table :'users'},
+                    (payload) => {
+                    if(payload.errors === "null")
+                        {
+                            Alert.alert('Update', 'User Updated Successfully')
+                            this.setState({
+                                editing : false,
+                                name : uname,
+                                username : uusername,
+                                admin : uadmin,
+                                address : uaddress,
+                                phone : uphone
+
+                            })
+                        }
+                    }).subscribe()
+        }
+
         return(
             <View style={{flex : 1, backgroundColor : 'white', justifyContent : 'flex-start', alignItems : 'flex-start'}}>
                <View style={{ flexDirection: 'row', justifyContent: 'flex-start', alignItems: 'center', padding: 10, marginTop: 20, width: width }}>
@@ -164,7 +303,7 @@ class EditUser extends React.Component {
                     </TouchableOpacity>
                     <Text style={{ fontFamily: 'InterTight-Bold', fontSize: 30, color: 'black', marginTop: 15, marginLeft: 25 }}>Edit User</Text>                
                 </View>
-                <ScrollView contentContainerStyle={{alignItems : 'center', paddingBottom : 30}}>
+                <ScrollView contentContainerStyle={{alignItems : 'center', alignSelf:'center', paddingBottom : 30}}>
                     <Lottie style={{height : height/2,width : width, alignSelf : 'center'}} source={require('../assets/editing.json')} autoPlay loop />
                      
                         <TouchableOpacity disabled style={{height : 50, width : 150, backgroundColor : '#add8e5',marginLeft : 30, borderRadius : 10, justifyContent : 'center', alignItems : 'center', alignSelf : 'flex-start'}} >
@@ -233,12 +372,14 @@ class EditUser extends React.Component {
                             </View>
                     </View>
 
-                    <View style={{flexDirection : 'row', alignSelf : 'center', marginTop : 20}}>
-                        <TouchableOpacity style={{width : width - 100, backgroundColor : 'white', marginLeft : 10, height : 50, justifyContent : 'center', alignItems : 'center'}} onPress={()=>{}}>
+                    <View style={{alignSelf : 'center', marginTop : 30}}>
+                        <TouchableOpacity disabled={!this.state.editing} style={{width : width - 100, backgroundColor : 'white', marginLeft : 30, height : 50, justifyContent : 'center', alignItems : 'center', alignSelf : 'center',borderRadius : 10, elevation : 5, borderWidth : 0.5}} onPress={()=>{updateData(
+                            this.state.name,this.state.username,this.state.admin,this.state.address,this.state.phone
+                        )}}>
                             <Text style={{fontFamily : 'Outfit-SemiBold', fontSize : 15}}>Update Data</Text>
                         </TouchableOpacity>
                     </View>
-
+                    
                 </ScrollView>
             </View>
         )
